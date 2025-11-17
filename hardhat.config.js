f3aebf7f317b1a96c49a241bb61581a9edf86d83
separchael <snaps.pout_5b@icloud.com>
2025-11-17 10:00:00 +0800

    init hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@fhevm/hardhat-plugin");
+require("dotenv/config");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+      evmVersion: "cancun",
+      viaIR: true,
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
